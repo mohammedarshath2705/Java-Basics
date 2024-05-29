87999427e9ebc0a768d278796554048775b507b8
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-05-29 21:59:07 +0530

    oops started

---
+++ oops/Firstone.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class Firstone {
+	static void Mymethod() {
+		int age = 23;
+		int NewAge = age +5;
+		System.out.println("age is  " +NewAge);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Mymethod();
+
+	}
+
+}
