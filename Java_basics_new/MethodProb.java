37199350e5b21fe39edd38ddb79d70b18a340ae2
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-05-31 21:26:12 +0530

    solved little methods problem

---
+++ Java_basics_new/MethodProb.java
@@ -0,0 +1,76 @@
+package project;
+
+import java.util.Scanner;
+
+public class MethodProb {
+	       //max of 2 no
+	/* static int max(int x,int y) {
+		if(x>y) {
+			return x;
+		}
+		else
+			return y;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int M,N,C;
+		System.out.print("Enter the First no : ");
+		M=sc.nextInt();
+		System.out.print("Enter the second no : ");
+		N=sc.nextInt();
+		C=max(M,N);
+		System.out.println("The greater is " +C);
+		sc.close();
+		
+
+	} */
+	//changing the array value using method
+	/*
+	static void update(int a[]) {
+		a[2]=23;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a[] = new int[5];
+		System.out.println("Enter the elements : ");
+		for(int i=0;i<5;i++) {
+			
+			a[i] = sc.nextInt();
+		}
+		System.out.println();
+		for(int x:a) {
+			System.out.print(x + " ");
+		}
+		update(a);
+		System.out.println();
+		for(int x:a) {
+			System.out.print(x + " ");
+		}
+		sc.close();
+	}
+	*/
+	
+	// username of mailid
+	
+	static String username(String mailid) {
+		int a =mailid.indexOf("@");
+		String N = mailid.substring(0,a);
+		return N;
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the mailid : ");
+		String mailid = sc.next();
+		String name = username(mailid);
+		System.out.println("The name of mail id is " +name);
+		sc.close();
+		
+	}
+	
+	
+
+}
