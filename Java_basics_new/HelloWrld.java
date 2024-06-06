18d66433839d3580ab15a7258d1dd959bc96268a
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-06-06 22:19:38 +0530

    06/06

---
+++ Java_basics_new/HelloWrld.java
@@ -0,0 +1,19 @@
+package project;
+//import java.util.Arrays;
+import oops.NewPackages;
+
+public class HelloWrld {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+	//	String arr[] = {"Arshath","Tariq","Rafiq","Niyaz","Farith","Rashith"};
+	//	Arrays.sort(arr);
+	//	for(String X : arr) {
+	//		System.out.println(X);
+	//	}
+		String name = "Naveen";
+		NewPackages obj = new NewPackages();
+	      
+	      obj.show(name);
+	}
+}
