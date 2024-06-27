6c8a9224963fecf30b9537585007a740feae5135
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-06-27 21:49:40 +0530

    Prefix

--- oops/ProblemIn.java
+++ oops/ProblemIn.java
@@ -313,7 +313,7 @@ public class ProblemIn {
 	}
 	*/
 //primefactor
-public class ProblemIn {
+/*public class ProblemIn {
 	static void PrimeFactor(int n) {
 		if(n <= 1)
 			return;
@@ -358,5 +358,23 @@ public class ProblemIn {
 		int x = sc.nextInt();
 		PrimeFactor(x);
 		sc.close();
+	}
+}
+*/
+//prefix
+public class ProblemIn {
+	public String longestCommonPrefix(String[] S) {
+		if (S.length == 0) return "";
+		String prefix = S[0];
+		for (int i = 1; i < S.length; i++)
+			while (S[i].indexOf(prefix) != 0) {
+				prefix = prefix.substring(0, prefix.length() - 1);
+				if (prefix.isEmpty()) return "";
+			}        
+		return prefix;}
+	public static void main(String[] args) {
+		ProblemIn ob = new ProblemIn();
+		String s[] = {"flow", "flower", "flame"};
+		System.out.println(ob.longestCommonPrefix(s));
 	}
 }
