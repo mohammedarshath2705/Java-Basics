24f45d5d905c6ee942d5c9fc55d5ec47e1552a71
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-07-06 22:43:11 +0530

    Linear Search

--- oops/ProblemIn.java
+++ oops/ProblemIn.java
@@ -445,6 +445,7 @@ public class ProblemIn {
     }
 }
 */
+/*
 interface SumCalculator {
     int sum(int a, int b);
 }
@@ -457,3 +458,27 @@ public class ProblemIn {
 	        System.out.println("Sum 15, -35): " + result);
 	 }
 }
+*/
+
+
+public class ProblemIn {
+	static int search(int arr[], int n, int x) {
+		for (int i = 0; i < n; i++) { 
+			 if (arr[i] == x) 
+	                return i; 
+		}
+		return -1; 
+	}
+	 public static void main(String[] args) {
+		 int[] arr = { 3, 4, 1, 7, 4,3,3,3,4,3,2,2,25 }; 
+	        int n = arr.length; 
+	        
+	        int x = 3;
+	  
+	        int index = search(arr, n, x); 
+	        if (index == -1) 
+	            System.out.println("Element is not present in the array"); 
+	        else
+	            System.out.println("Element found at position " + index); 
+	 }
+	 }
