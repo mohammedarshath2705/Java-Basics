fa2bdbf4291d6aed387aa9bd854445f81f475ffa
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-08-07 21:41:57 +0530

    Majority Element

--- DSA/MajorityElement/src/Main.java
+++ DSA/MajorityElement/src/Main.java
@@ -1,3 +1,10 @@
+
+
+/*Given an array A of N elements.
+Find the majority element in the array.
+A majority element in an array A of size N is an element that appears strictly more than N/2 times in the array.*/
+
+
 public class Main {
     static int majorityElement(int[] a, int size) {
 
