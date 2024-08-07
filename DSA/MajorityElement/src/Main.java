cc75a261af61cbce1294245556cc2594a1d47de5
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-08-07 21:37:11 +0530

    Majority Element

---
+++ DSA/MajorityElement/src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    static int majorityElement(int[] a, int size) {
+
+        int candidate = findCandidate(a, size);
+
+
+        if (isMajority(a, size, candidate)) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
+
+    private static int findCandidate(int[] a, int size) {
+        int candidate = -1;
+        int count = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (count == 0) {
+                candidate = a[i];
+                count = 1;
+            } else if (a[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    private static boolean isMajority(int[] a, int size, int candidate) {
+        int count = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (a[i] == candidate) {
+                count++;
+            }
+        }
+
+        return count > size / 2;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 2, 1, 1, 1, 2, 2};
+        int size = array.length;
+        int majority = Main.majorityElement(array, size);
+        System.out.println("Majority element is: " + majority);
+    }
+}
