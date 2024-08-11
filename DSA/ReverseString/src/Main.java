fcc8d2fc16db0f6d91c8735cf6385d6af901ed4c
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-08-11 23:14:56 +0530

    Reverse a String

---
+++ DSA/ReverseString/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter a String : ");
+        String a=s.nextLine();
+        StringBuilder b= new StringBuilder();
+        int ptr=a.length()-1;
+        while(ptr>=0){
+            b.append(a.charAt(ptr));
+            ptr--;
+        }
+        System.out.println(b);
+    }
+}
